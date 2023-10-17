4a7bc3fad086e8b27e55035f88071e0fe44465b2
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-10-17 19:36:14 -0300

    Implementado o componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <img src="/src/assets/nobel_logo.png" alt="Nobel Logo" />
+      </header>
+    );
+  }
+}
+
+export default Header;
